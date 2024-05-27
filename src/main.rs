d17acfa8d98a18032e33c6ae5d388bc010dc1b71
jdolan44 <Jdolan16646@gmail.com>
2024-05-26 21:36:24 -0400

    completed guessing game and created a helper function for comparing guesses

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    loop {
+        println!("Enter your guess:");
 
-    println!("Enter your guess:");
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
-    
-    println!("you guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("invalid input! try again."); 
+                continue;
+            }
+        };
+        
+        println!("you guessed: {guess}");
+
+        let has_won = comp_guess(secret_number, guess);
+        if has_won { break; }
+    }
+}
+
+//function that compares the guess and the secret number.
+//displays a message and returns true if they are equal.
+fn comp_guess(secret_number: u32, guess: u32) -> bool{
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+            println!("You win!");
+            return true;
+        }
+    }
+    return false;
 }
